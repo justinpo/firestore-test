3fc9c86a5a9cda7ad6ea69ea13f24942af78f0c2
Justin Po <justin.andre.po@gmail.com>
2020-05-12 16:13:09 +0800

    parses quantity value

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const itemList = document.getElementById("item-list");
 const form = document.getElementById("add-item-form");
 
-const renderItem = (doc) => {
+renderItem = doc => {
   const item = document.createElement("li");
   const name = document.createElement("span");
   const quantity = document.createElement("span");
@@ -18,35 +18,33 @@ const renderItem = (doc) => {
 
   itemList.appendChild(item);
 
-  remove.addEventListener("click", (e) => {
+  remove.addEventListener("click", e => {
     const id = e.target.parentElement.getAttribute("data-id");
     db.collection("items").doc(id).delete();
-  });
-};
+  })
+}
 
-// db.collection("items")
-//   .get()
-//   .then((snapshot) => {
-//     snapshot.docs.forEach((doc) => {
-//       renderItem(doc);
-//     });
-//   });
+// db.collection('items').orderBy("name", "desc").get().then(snapshot => {
+//   snapshot.docs.forEach(doc => {
+//     renderItem(doc);
+//   })
+// })
 
-form.addEventListener("submit", (e) => {
+form.addEventListener("submit", e => {
   e.preventDefault();
   db.collection("items").add({
     name: form.name.value,
-    quantity: form.quantity.value,
-  });
+    quantity: parseInt(form.quantity.value),
+  })
   form.name.value = "";
   form.quantity.value = "";
-});
+})
 
 db.collection("items").onSnapshot(snapshot => {
   const changes = snapshot.docChanges();
 
   changes.forEach(change => {
-    if (change.type === "added") {
+    if(change.type === "added") {
       renderItem(change.doc);
     } else if (change.type === "removed") {
       const item = itemList.querySelector("[data-id=" + change.doc.id + "]");
